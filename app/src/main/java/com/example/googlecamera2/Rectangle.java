831fd44e5b9087edbed0d28cba8bfc7d8978907b
Vaishnavi Killekar <vaishnavibgm@gmail.com>
2019-03-07 09:42:20 +0530

    Overlay commit

---
+++ app/src/main/java/com/example/googlecamera2/Rectangle.java
@@ -0,0 +1,29 @@
+package com.example.googlecamera2;
+
+/**
+ * Created by Vaishnavi Killekar on 06-Mar-19.
+ */
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.view.View;
+
+public class Rectangle extends View {
+    Paint paint = new Paint();
+
+    public Rectangle(Context context) {
+        super(context);
+    }
+
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        paint.setColor(Color.GREEN);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(6);
+        Rect rect = new Rect(120, 100, 620, 900);
+        canvas.drawRect(rect, paint );
+    }
+}
