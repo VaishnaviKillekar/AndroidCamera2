c6c7fe0478c90001f432a622a023ec687cdfe7e9
Vaishnavi Killekar <vaishnavibgm@gmail.com>
2019-03-12 17:04:52 +0530

    Image cropping and display done

--- app/src/main/java/com/example/googlecamera2/Rectangle.java
+++ app/src/main/java/com/example/googlecamera2/Rectangle.java
@@ -8,22 +8,34 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.util.Log;
 import android.view.View;
 
 public class Rectangle extends View {
     Paint paint = new Paint();
+    int width;
+    int height;
+    boolean camera_status;
 
-    public Rectangle(Context context) {
+    public Rectangle(Context context, boolean camera_status, int width, int height) {
         super(context);
+        this.camera_status = camera_status;
+        this.width = width;
+        this.height = height;
     }
 
 
     @Override
     public void onDraw(Canvas canvas) {
+        if(!camera_status)
+            return;
+
         paint.setColor(Color.GREEN);
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeWidth(6);
-        Rect rect = new Rect(120, 100, 620, 900);
-        canvas.drawRect(rect, paint );
+
+        Log.e("RECT", width +" "+ height);
+        Rect rect = new Rect(width/8, height/8, 7*width/8, 7*height/8);
+        canvas.drawRect(rect, paint);
     }
 }
